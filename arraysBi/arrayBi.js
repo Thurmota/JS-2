7e77393c5afb97ab2614bdf9805fb51dcedda14b
Thurmota <arthurpachecomota@gmail.com>
2022-09-30 14:17:21 -0300

    Inclusao de arrays bidimensionais

---
+++ arraysBi/arrayBi.js
@@ -0,0 +1,16 @@
+const arr = [
+    "1 Nível",
+    "2 Nível",
+    "3 Nível"
+]
+
+// Array dentro de array = array bidimensional
+
+console.log(arr)
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[1][0])
+console.log(arr[1][1])
+console.log(arr[2])
+console.log(arr[2][1])
+console.log(arr[2][1][0])
